8082cbc8e597cc5b45c8a259132d709456c1622e
Patryk <patryk.r.nowak@gmail.com>
2022-12-05 09:35:42 +0000

    displaying post with images from database

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include "includes/header.php" ?>
+<!-- // from here we are getting $connection variable -->
 <?php include "includes/db.php" ?>
 
 <!-- Navigation -->
@@ -12,31 +13,57 @@
 
         <!-- Blog Entries Column -->
 
-
-
-
         <div class="col-md-8">
 
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
+
+            <?php
+            $query = "SELECT * FROM posts";
+
+            $select_all_posts_query = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                // reading from the db
+
+                $post_title = $row["post_title"];
+                $post_author = $row["post_author"];
+                $post_date = $row["post_date"];
+                $post_image = $row["post_image"];
+                $post_content = $row["post_content"];
+
+                // this is such a strange syntax in one way!
+            ?>
+                <!-- DYNAMIC PART -> HTML -->
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="Image of the post">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+
+                <!-- CLOSING BRACKET IN PHP -->
+            <?php   }  ?>
+
+
+
+
+
+
+
 
             <!-- First Blog Post -->
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-            <hr>
 
 
 
@@ -59,7 +86,7 @@
 
         <!-- Blog Sidebar Widgets Column -->
         <?php include "includes/sidebar.php" ?>
-         
+
     </div>
     <!-- /.row -->
 
