eee5e6a226cfd5dbdcaca152315ec6f8bfb6d444
Patryk <patryk.r.nowak@gmail.com>
2022-12-20 11:34:43 +0000

    another version of users online count <- JS plus $.get ajax request to php file <- interesting

--- admin/functions.php
+++ admin/functions.php
@@ -1,27 +1,62 @@
 <?php
 function users_online()
 {
-    // changing the $connection variable to the global scope
-    global $connection;
+    if (isset($_GET['onlineusers'])) {
 
-    $session = session_id(); // it will have id of all users logged in (id of session)
-    $time = time();
-    $time_out_in_seconds = 60;
-    $time_out = $time - $time_out_in_seconds;
+        global $connection;
 
-    $query = "SELECT * FROM users_online WHERE session = '$session' ";
-    $send_query = mysqli_query($connection, $query);
-    $count = mysqli_num_rows($send_query);
+        if (!$connection) {
 
-    if ($count == NULL) {
-        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', $time) ");
-    } else {
-        mysqli_query($connection, "UPDATE users_online SET time = $time WHERE session = '$session' ");
+            session_start();
+
+            include("../includes/db.php");
+
+            $session = session_id();
+            $time = time();
+            $time_out_in_seconds = 05;
+            $time_out = $time - $time_out_in_seconds;
+
+            $query = "SELECT * FROM users_online WHERE session = '$session'";
+            $send_query = mysqli_query($connection, $query);
+            $count = mysqli_num_rows($send_query);
+
+            if ($count == NULL) {
+
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
+            } else {
+
+                mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+            }
+
+            $users_online_query =  mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+            echo $count_user = mysqli_num_rows($users_online_query);
+        }
+    } // get request isset()
+    else {
+        // changing the $connection variable to the global scope
+        global $connection;
+
+        $session = session_id(); // it will have id of all users logged in (id of session)
+        $time = time();
+        $time_out_in_seconds = 60;
+        $time_out = $time - $time_out_in_seconds;
+
+        $query = "SELECT * FROM users_online WHERE session = '$session' ";
+        $send_query = mysqli_query($connection, $query);
+        $count = mysqli_num_rows($send_query);
+
+        if ($count == NULL) {
+            mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', $time) ");
+        } else {
+            mysqli_query($connection, "UPDATE users_online SET time = $time WHERE session = '$session' ");
+        }
+
+        $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > $time_out");
+        return $count_user = mysqli_num_rows($users_online_query);
     }
-
-    $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > $time_out");
-    return $count_user = mysqli_num_rows($users_online_query);
 }
+
+users_online();
 
 function insert_categories()
 {
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -26,6 +26,23 @@ var div_box = "<div id='load-screen'><div id='loading'></div></div>";
 
 $("body").prepend(div_box);
 
-$('#load-screen').delay(700).fadeOut(600, function(){
-   $(this).remove();
-});
+$("#load-screen")
+  .delay(700)
+  .fadeOut(600, function () {
+    $(this).remove();
+  });
+
+function loadUsersOnline() {
+  // TODO: interesting part - will it call other functions in the functions.php file?
+  // https://code.tutsplus.com/tutorials/how-to-use-ajax-in-php-and-jquery--cms-32494   <-- nice
+  // https://www.w3schools.com/jquery/ajax_get.asp
+  $.get("functions.php?onlineusers=result", function (data) {
+    // injecting values - this is fine
+    $(".usersonline").text(data);
+  });
+}
+
+loadUsersOnline();
+setInterval(function () {
+  loadUsersOnline();
+}, 60000);
